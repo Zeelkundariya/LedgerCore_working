43df938d54777ea2535465c9e16afe953e123325
Zeelkundariya <zeel.kundariya.m.cg@gmail.com>
2026-08-01 23:46:32 +0530

    Push 6: Implement User Authentication routes (JWT & bcrypt)

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const { notFound, errorHandler } = require('./middleware/errorMiddleware');
+const authRoutes = require('./routes/authRoutes');
 
 dotenv.config();
 connectDB();
@@ -12,6 +13,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
   res.send('Skill Swap API is running!');
