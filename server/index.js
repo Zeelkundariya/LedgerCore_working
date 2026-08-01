45f93f15c5178d1643cb862b3c81fc141f5bc063
Zeelkundariya <zeel.kundariya.m.cg@gmail.com>
2026-08-01 23:34:15 +0530

    Push 2: Setup MongoDB mongoose connection and error handling

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const { notFound, errorHandler } = require('./middleware/errorMiddleware');
 
 dotenv.config();
+connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -13,6 +16,9 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Skill Swap API is running!');
 });
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
