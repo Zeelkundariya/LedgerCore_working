22af4a326e9f0a62a49abd0d13ba889b73284506
Zeelkundariya <zeel.kundariya.m.cg@gmail.com>
2026-08-01 23:51:38 +0530

    Push 11: Implement Admin routes (list users, ban user, moderate bio)

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const { notFound, errorHandler } = require('./middleware/errorMiddleware');
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
 const swapRoutes = require('./routes/swapRoutes');
+const adminRoutes = require('./routes/adminRoutes');
 
 dotenv.config();
 connectDB();
@@ -19,6 +20,7 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/swaps', swapRoutes);
+app.use('/api/admin', adminRoutes);
 
 app.get('/', (req, res) => {
   res.send('Skill Swap API is running!');
